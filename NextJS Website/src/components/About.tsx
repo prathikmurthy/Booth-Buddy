2079fcb9a066eadd032457d635fea25d7ddc128f
Blake Potvin <58959047+Prodxgy@users.noreply.github.com>
2023-01-29 04:56:09 -0500

    fixed padding

--- NextJS Website/src/components/About.tsx
+++ NextJS Website/src/components/About.tsx
@@ -1,26 +1,26 @@
 const About = () => {
   return (
     <div className="text-white container mx-auto py-4 h-full">
-      <h1 className="text-6xl font-bold pb-3 mb-5">What is BoothBuddy?</h1>
-      <p className="text-lg w-[60%] mb-52">
+      <h1 className="text-6xl font-bold pb-3 mb-5  px-5">What is BoothBuddy?</h1>
+      <p className="text-lg w-[60%] mb-52  px-5">
         Created with sustainability in mind, BoothBuddy is a platform for
         sharing club and recruiter information without the need of paper flyers.
       </p>
       <div className="flex flex-col items-end text-right">
-        <h1 className="text-6xl font-bold pb-3 mb-5">
+        <h1 className="text-6xl font-bold pb-3 mb-5 px-5">
           Why Was BoothBuddy Created?
         </h1>
-        <p className="text-lg w-[60%] mb-52">
+        <p className="text-lg w-[60%] mb-52  px-5">
           BoothBuddy aimed to solve two issues. One, the amount of paper flyers
           printed that are wasted and thrown away. Two, the lack of cell
           reception during the fair. BoothBuddy allows you to view all the
           information you need without the need of paper flyers.
         </p>
       </div>
-      <h1 className="text-6xl font-bold pb-3 mb-5">
+      <h1 className="text-6xl font-bold pb-3 mb-5  px-5">
         How Does BoothBuddy Work?
       </h1>
-      <p className="text-lg w-[60%] mb-52">
+      <p className="text-lg w-[60%] mb-52  px-5">
         BoothBuddy aimed to solve two issues. One, the amount of paper flyers
         printed that are wasted and thrown away. Two, the lack of cell reception
         during the fair. BoothBuddy allows you to view all the information you
