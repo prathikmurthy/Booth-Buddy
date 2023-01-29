9fdd728d84636cafd03c3100b99474401c20faa9
ledanka <ledan3@msu.edu>
2023-01-28 22:31:03 -0500

    edited form

--- Flutter App/booth_buddy/lib/screens/form.dart
+++ Flutter App/booth_buddy/lib/screens/form.dart
@@ -3,6 +3,9 @@ import 'package:flutter/material.dart';
 class form extends StatelessWidget {
   final nameController = TextEditingController();
   final emailController = TextEditingController();
+  final majorController = TextEditingController();
+  final yearController = TextEditingController();
+  final phoneController = TextEditingController();
 
   //form({super.key});
 
@@ -35,7 +38,21 @@ class form extends StatelessWidget {
                       labelText: "Email",
                       prefixIcon: Icon(Icons.email_outlined),
                       border: OutlineInputBorder(),
-                    ))
+                    )),
+                TextFormField(
+                    controller: majorController,
+                    decoration: InputDecoration(
+                      labelText: "Major",
+                      prefixIcon: Icon(Icons.school_outlined),
+                      border: OutlineInputBorder(),
+                    )),
+                TextFormField(
+                    controller: yearController,
+                    decoration: InputDecoration(
+                      labelText: "School Year",
+                      prefixIcon: Icon(Icons.calendar_today),
+                      border: OutlineInputBorder(),
+                    )),
               ]))),
     );
   }
