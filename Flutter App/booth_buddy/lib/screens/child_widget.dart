55f9eb2830b6d8a1e61fdb45a8b6654b3090b729
Sampath Eaty <s.eaty6@gmail.com>
2023-01-28 20:22:28 -0500

    Sample sliding screens with bottom navbar
    Co-authored-by: ledanka <ledanka@users.noreply.github.com>

---
+++ Flutter App/booth_buddy/lib/screens/child_widget.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class ChildWidget extends StatelessWidget {
+  final AvailableNumber number;
+
+  const ChildWidget({Key? key, required this.number}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    String file = "";
+    if (number == AvailableNumber.First) {
+      file = "first";
+    } else if (number == AvailableNumber.Second) {
+      file = "second";
+    } else {
+      file = "third";
+    }
+
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "$file screen",
+              style: TextStyle(fontSize: 36.0),
+            ),
+            Flexible(child: Image.asset("assets/undraw/$file.png"))
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+enum AvailableNumber { First, Second, Third }
---
+++ Flutter App/booth_buddy/lib/screens/home_screen.dart
@@ -0,0 +1,81 @@
+import 'package:booth_buddy/screens/child_widget.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  PageController _pageController = PageController(
+    initialPage: 0,
+  );
+  int currentIndex = 0;
+
+  Widget childWidget = ChildWidget(
+    number: AvailableNumber.First,
+  );
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Theme.of(context).primaryColor,
+        unselectedItemColor: Colors.grey[500],
+        currentIndex: currentIndex,
+        onTap: (value) {
+          currentIndex = value;
+          _pageController.animateToPage(
+            value,
+            duration: Duration(milliseconds: 200),
+            curve: Curves.linear,
+          );
+
+          setState(() {});
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            //title: Text("First"),
+            label: "First",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.trending_up),
+            //title: Text("Second"),
+            label: "Second",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.dashboard),
+            //title: Text("Third"),
+            label: "Third"
+          ),
+          // BottomNavigationBarItem(
+          //   icon: Icon(Icons.dashboard),
+          //   //title: Text("Third"),
+          //   label: "Third"
+          // ),
+        ],
+      ),
+      body: PageView(
+        controller: _pageController,
+        onPageChanged: (page) {
+          setState(() {
+            currentIndex = page;
+          });
+        },
+        children: <Widget>[
+          ChildWidget(number: AvailableNumber.First),
+          ChildWidget(number: AvailableNumber.Second),
+          ChildWidget(number: AvailableNumber.Third),
+          // ChildWidget(number: AvailableNumber.Third)
+        ],
+      ),
+    );
+  }
+}
